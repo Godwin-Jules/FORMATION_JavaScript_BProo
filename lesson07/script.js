384d615e450fcf1b40c367a9335f0d8c40ec1d91
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-02-05 21:16:37 +0000

    Completing lesson_07 : Variable type {UNDEFINED, NULL, NaN, boolean} (with BProo)

---
+++ lesson07/script.js
@@ -0,0 +1,13 @@
+// Les variables de type NULL, UNDEFINED,NaN (Not a number), booleans
+
+let age;
+console.log(`Mon âge est : ${age}, et d'ici 5 ans, j'aurais : ${age + 5}`);
+
+let donne = null;
+console.log(`La donnée est : ${age}, en ajoutant 5 on a : ${age + 5}`);
+
+let datas = [true, false, 'true', 'false'];
+console.log(datas);
+
+let email = 'julesdieudonne97@gmail.com';
+console.log(email.includes("@"));
