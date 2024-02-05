3dbcd6d84801253f5fedadf3a9885d972c67aa78
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-02-05 20:42:42 +0000

    Completing lesson_05 : Arrays (with BProo)

---
+++ lesson05/script.js
@@ -0,0 +1,12 @@
+// array -> tableau
+let fruits = ['banane', 'ananas', 'avocat', 'papaye', 'mangue'];
+console.log(fruits);
+fruits[2] = 'goyave';
+console.log(fruits);
+fruits.push('cerise');
+console.log(fruits);
+
+let array_aleatoire = ['milegne', 'ananas', 25, true, 'good', ['a', 1]];
+
+console.log(array_aleatoire);
+console.log(array_aleatoire.length);
