6103e373814b1c8a0c1bfdd6559252b60c15a63e
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-02-10 09:02:43 +0000

    The last part : Tips - 07 over 10 (with BProo)

---
+++ tips/tip07/script.js
@@ -0,0 +1,9 @@
+/** Tip 07
+ * Double exclamation Operator !!
+*/
+// permet de convertir le résulttat d'une expression en un type boolean
+
+const greeting = 'Hello there!';
+console.log(!!greeting);    // returns true
+const noGreeting = '';
+console.log(!!noGreeting);  // returns false
