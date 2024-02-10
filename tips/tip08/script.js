ec85439a9ac410c01a17aeb8b0a1b6dd30544ebe
MILEGNE Dieu donné <mdjulesdeveloper23@gmail.com>
2024-02-10 09:07:34 +0000

    The last part : Tips - 08 over 10 (with BProo)

---
+++ tips/tip08/script.js
@@ -0,0 +1,13 @@
+/** Tip 08
+ * Conversion de chaînes et d'entiers
+*/
+
+// Conversion de string en entier
+const stringNumber = '123';
+console.log(+stringNumber);
+console.log(typeof +stringNumber);
+
+// Conversio d'entier en string
+const myString = 25 + '';
+console.log(myString);
+console.log(typeof myString);
